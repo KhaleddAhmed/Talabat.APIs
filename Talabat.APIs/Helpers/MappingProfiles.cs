d68da039d29ff5036d21ff8cbfb1ef0c40569c4a
KhaleddAhmed <kh.ahmed.farouk@gmail.com>
2024-04-20 21:18:56 +0200

    06-Product Picture Url Resolver

--- Talabat.APIs/Helpers/MappingProfiles.cs
+++ Talabat.APIs/Helpers/MappingProfiles.cs
@@ -6,14 +6,18 @@ namespace Talabat.APIs.Helpers
 {
 	public class MappingProfiles:Profile
 	{
-        public MappingProfiles()
+	
+
+		public MappingProfiles()
         {
-            CreateMap<Product, ProductToReturnDto>()
-                .ForMember(d => d.Brand,O=>O.MapFrom(s=>s.Brand.Name))
-                .ForMember(d => d.Cateogry, O => O.MapFrom(s=>s.Cateogry.Name));
+			
 
+			CreateMap<Product, ProductToReturnDto>()
+				.ForMember(d => d.Brand, O => O.MapFrom(s => s.Brand.Name))
+				.ForMember(d => d.Cateogry, O => O.MapFrom(s => s.Cateogry.Name))
+				//.ForMember(P => P.PictureUrl, O => O.MapFrom(S => $"{_configuration["ApiBaseUrl"]}/{S.PictureUrl}"));
+				.ForMember(P => P.PictureUrl, O => O.MapFrom<ProductPictureUrlResolver>());
+		}
 
-
-        }
-    }
+	}
 }
