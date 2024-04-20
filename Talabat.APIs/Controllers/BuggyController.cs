5a690ff32c1da3844406637e5d6b32c0551dc13b
KhaleddAhmed <kh.ahmed.farouk@gmail.com>
2024-04-20 22:34:49 +0200

    08-ErrorTypes

---
+++ Talabat.APIs/Controllers/BuggyController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Infrastructure.Data;
+
+namespace Talabat.APIs.Controllers
+{
+
+	public class BuggyController : BaseApiController
+	{
+		private readonly StoreContext _dbContext;
+
+		public BuggyController(StoreContext dbContext) 
+		{
+			_dbContext = dbContext;
+		}
+
+		[HttpGet("notfound")]
+		public ActionResult GetNotFoundRequest()
+		{
+			var product = _dbContext.Products.Find(100);
+			if (product is null)
+			   return NotFound();
+			
+
+			return Ok(product);
+		}
+
+
+		[HttpGet("servererror")]
+
+		public ActionResult GetServerError()
+		{
+			var product = _dbContext.Products.Find(100);
+			var productToReturn=product.ToString(); //will Throw Non FRefrence Exception
+
+			return Ok(productToReturn);
+		}
+
+		[HttpGet("badrequest")]
+		public ActionResult GetBadRequest() 
+		{
+			return BadRequest();
+		}
+
+		[HttpGet("badrequest/{id}")]
+		public ActionResult GetBadRequest(int id) //Validation error lw b3t string bdal id
+		{
+			return Ok();
+		}
+
+
+
+
+	}
+}
