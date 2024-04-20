aef1fc4765d67d788cee00ae852df5f646a06f2e
KhaleddAhmed <kh.ahmed.farouk@gmail.com>
2024-04-20 18:58:34 +0200

    04-Draft Example using Specifications Dp

---
+++ Talabat.APIs/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories.Contract;
+using Talabat.Core.Specifications.Employee_Specs;
+
+namespace Talabat.APIs.Controllers
+{
+
+	public class EmployeeController : BaseApiController
+	{
+		private readonly IGenericRepository<Employee> _empRepo;
+
+		public EmployeeController(IGenericRepository<Employee> empRepo) 
+		{
+			_empRepo = empRepo;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+		{
+			var spec = new EmployeeWithDepartmentSpecifications();
+			var employees = await _empRepo.GetAllWithSpec(spec);
+
+			return Ok(employees);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Employee>> GetEmployee(int id)
+		{
+			var spec = new EmployeeWithDepartmentSpecifications(id);
+
+			var employee=_empRepo.GetWithSpec(spec);
+
+			if(employee is null)
+			  return NotFound();
+			
+
+			return Ok(employee);
+		}
+
+	}
+}
