194b2748743f00c21848bf48ea5da4872a9536ec
KhaleddAhmed <kh.ahmed.farouk@gmail.com>
2024-05-05 02:00:39 +0300

    Part03 Login EndPoint

---
+++ Talabat.APIs/DTOs/UserDto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Talabat.APIs.DTOs
+{
+	public class UserDto
+	{
+		public string DisplayName { get; set; } = null!;
+		public string Email { get; set; }= null!;
+        public string Token { get; set; } = null!;
+    }
+}
