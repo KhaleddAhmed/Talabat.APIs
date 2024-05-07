f73ab9237cd10513e7b496b3222166560a6eb49f
KhaleddAhmed <kh.ahmed.farouk@gmail.com>
2024-05-07 19:43:34 +0300

    Part01-Order Module-Entities

---
+++ Talabat.Core/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities.OrderAggregate
+{
+	public class Order:BaseEntity
+	{
+        public string BuyerEmail { get; set; }
+
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.UtcNow;
+
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+
+        public Address Address { get; set; } = null!;
+
+        //public int DeliveryMethodId { get; set; }//Foreign Key
+
+        public DeliveryMethod DeliveryMethod { get; set; } //Navigational property One
+
+        public ICollection<OrderItem> OrderItems { get; set; }=new HashSet<OrderItem>();
+
+        public decimal SubTotal { get; set; }
+        //[NotMapped]
+        //public decimal Total { get { return DeliveryMethod.Cost + SubTotal; } }
+
+        public decimal GetTotal()=>SubTotal+DeliveryMethod.Cost;
+
+        public string PaytmentIntent { get; set; } = string.Empty;
+
+    }
+}
