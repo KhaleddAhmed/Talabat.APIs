70e5e51a4ad3cbebfce9364d5fdfbb50923b02d4
KhaleddAhmed <kh.ahmed.farouk@gmail.com>
2024-05-08 00:05:20 +0300

    Part05-Order Service

---
+++ Talabat.Core/Services.Contract/IOrderService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.OrderAggregate;
+
+namespace Talabat.Core.Services.Contract
+{
+	public interface IOrderService
+	{
+		Task<Order> CreateOrderAsync(string buyerEmail,string deliveryMethodId,string basketId,Address shippingAddress);
+
+		Task<IReadOnlyList<Order>> GetOrdersFouUserAsync(string buyerEmail);
+
+		Task<Order> GetOrderByIdForUserAsync(string buyerEmail,int orderId);
+
+		Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsForUserAsync();
+	}
+}
---
+++ Talabat.Service/OrderService/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.OrderAggregate;
+using Talabat.Core.Services.Contract;
+
+namespace Talabat.Service.OrderService
+{
+	public class OrderService : IOrderService
+	{
+		public Task<Order> CreateOrderAsync(string buyerEmail, string deliveryMethodId, string basketId, Address shippingAddress)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsForUserAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Order> GetOrderByIdForUserAsync(string buyerEmail, int orderId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IReadOnlyList<Order>> GetOrdersFouUserAsync(string buyerEmail)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
